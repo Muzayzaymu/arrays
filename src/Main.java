a50922706bbfa00888b0ba2ba768e0a976e71d3d
Deru <muzayzaymu@gmail.com>
2024-12-04 20:48:22 +0500

    start homework

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Task 1
+        int [] anIntegerArray = new int[3];
+        double [] fractionalArray = {1.57, 7.654, 9.986};
+        int [] anArbitraryArray = {8, 12, 16, 20, 24};
+        for (int i = 0; i < anIntegerArray.length; i++){
+            anIntegerArray [i] = i + 1;
+        }
+    }
+}
